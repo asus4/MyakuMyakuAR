e2148e52e5b6b86c6113d837a59f37353ad7da38
Koki Ibukuro <koki.ibukuro@gmail.com>
2025-03-21 16:50:30 +0100

    Emit VFX from detected objects

--- Assets/Scripts/Runtime/MainController.cs
+++ Assets/Scripts/Runtime/MainController.cs
@@ -1,17 +1,37 @@
 using UnityEngine;
+using UnityEngine.VFX;
 
 namespace MyakuMyakuAR
 {
+    [RequireComponent(typeof(Yolo11SegARController))]
     sealed class MainController : MonoBehaviour
     {
+        [SerializeField]
+        VisualEffect vfx;
+
+        readonly int _SegmentationTex = Shader.PropertyToID("_SegmentationTex");
+        readonly int _ARRgbDTex = Shader.PropertyToID("_ARRgbDTex");
+
         void OnEnable()
         {
-            Debug.Log("MainController enabled");
+            if (TryGetComponent(out Yolo11SegARController yolo11Seg))
+            {
+                yolo11Seg.OnDetect += OnDetect;
+            }
         }
 
         void OnDisable()
         {
+            if (TryGetComponent(out Yolo11SegARController yolo11Seg))
+            {
+                yolo11Seg.OnDetect -= OnDetect;
+            }
         }
 
+        void OnDetect(Yolo11SegARController yolo11Seg)
+        {
+            vfx.SetTexture(_SegmentationTex, yolo11Seg.SegmentationTexture);
+            vfx.SetTexture(_ARRgbDTex, yolo11Seg.ARCameraTexture);
+        }
     }
 }
